0167dd370b121cfa7fdf0cdc503cf837cb85d41f
Owen Bellish <shadowguy224@live.com>
2019-03-14 08:43:23 -0400

    updated typings

--- typings/JsonMap.d.ts
+++ typings/JsonMap.d.ts
@@ -1,1 +1,5 @@
-export {};
+import { JsonMapOptions } from './Interfaces';
+export default class JsonMap extends Map {
+    constructor(iterable: any, options?: JsonMapOptions);
+    private [_defineSetting];
+}
